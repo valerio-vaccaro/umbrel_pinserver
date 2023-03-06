7f103c15798141100d39dfaca3d337fba9be8c9a
Valerio Vaccaro <valerio@blockstream.com>
2023-03-06 23:20:58 +0100

    env vars

---
+++ exports.sh
@@ -0,0 +1,6 @@
+export APP_PINSERVER_PORT="8096"
+export APP_PINSERVER_WEB_PORT="8081"
+
+set app_pinserver_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rpc/hostname"
+
+export APP_PINSERVER_HIDDEN_SERVICE="$(cat "${app_pinserver_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
