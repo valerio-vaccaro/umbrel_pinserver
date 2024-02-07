84175cc6acc444e3f200f1cb6dc19595ac783204
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2024-02-07 15:23:43 +0100

    update image and migrate port from 8096 to 8097

--- exports.sh
+++ exports.sh
@@ -1,4 +1,4 @@
-export APP_PINSERVER_PORT="8096"
+export APP_PINSERVER_PORT="8097"
 export APP_PINSERVER_WEB_PORT="8095"
 local app_pinserver_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}/hostname"
 
